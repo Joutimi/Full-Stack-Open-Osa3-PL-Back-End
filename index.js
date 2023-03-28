79d62cf958e65292dad008514c4b575be25042b7
Joutimi <timijouppila@hotmail.com>
2023-03-28 14:08:50 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
 const express = require('express')
 const app = express()
 
+require('dotenv').config()
+
 const morgan = require('morgan')
 const cors = require('cors')
+
+const Person = require('./models/person')
 
 app.use(express.json())
 morgan.token('reqBody', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'))
 app.use(cors())
+
 app.use(express.static('build'))
+const { response } = require('express')
 
 //Kovakoodattu lista ihmisistä
-let persons = [
+/* let persons = [
     {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
+
     }
-  ]
+  ]   */
 
   // Kutsu juureen tulostaa "Hello World"
   app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
   })
-
-  // api/persons palautaa json-muotoisen listan henkilöistä
-  app.get('/api/persons', (req, res) => {
-    res.json(persons)
+  
+  // Palauttaa kaikki listassa olevat henkilöt
+  app.get('/api/people', (request, response) => {
+    Person.find({}).then(people => {
+      response.json(people)
+    })
   })
 
   // /info tulostaa henkilöiden määrän persons-listassa ja pyynnön aikaleiman
@@ -76,14 +67,8 @@ let persons = [
     res.status(204).end()
   })
 
-  // ID: generointi
-  const newId = () => {
-    const ID = Math.floor(Math.random() * 5000)
-    return (ID)
-  }
-
   // Henkilön lisäys
-  app.post('/api/persons/', (req, res) => {
+/*   app.post('/api/persons/', (req, res) => {
 
     const body = req.body
 
@@ -113,9 +98,9 @@ let persons = [
 
     persons = persons.concat(person)
     res.json(person)
-  })
+  }) */
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
